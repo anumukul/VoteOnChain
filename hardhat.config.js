0d8118b034ecaae4936b1baf0dfe346597e909f6
Anubhav Singh <anubhavsingh@Mac.lan>
2026-02-07 03:29:26 +0530

    deployed the contracts

--- hardhat.config.js
+++ hardhat.config.js
@@ -22,17 +22,17 @@ module.exports = {
       url: "http://127.0.0.1:8545",
       chainId: 31337
     },
-    sepolia: {
-      url: process.env.SEPOLIA_RPC_URL,
-      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
-      chainId: 11155111
-    },
-    
+    ...(process.env.SEPOLIA_RPC_URL && {
+      sepolia: {
+        url: process.env.SEPOLIA_RPC_URL,
+        accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+        chainId: 11155111
+      }
+    })
   },
   etherscan: {
     apiKey: {
-      sepolia: process.env.ETHERSCAN_API_KEY,
-     
+      ...(process.env.ETHERSCAN_API_KEY && { sepolia: process.env.ETHERSCAN_API_KEY })
     }
   }
 };
